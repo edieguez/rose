03d7150b8050f7d479dd534b50ba87b8ad428786
edieguez <dieguezemmanuel@gmail.com>
2022-12-23 23:00:13 -0500

    Added support for search an arbitrary word

--- __main__.py
+++ __main__.py
@@ -1,23 +1,40 @@
 #! /usr/bin/env python
+import sys
+
+from cli import anki
 from database import dao
 from dictionary import free_dictionary
-from cli import anki
 
-print('Rose project')
 
-dao.create_initial_database()
-term = 'awe'
+def parse_arguments():
+    if len(sys.argv) <= 1:
+        print('No arguments provided')
+        sys.exit(1)
 
-result = free_dictionary.query(term)
-word = dao.get_word_by_text(result.text)
 
-if not word:
-    print(f'Word {term} not found in database')
+if __name__ == '__main__':
+    parse_arguments()
 
-    dao.insert_word(result)
+    dao.create_initial_database()
+    term = sys.argv[1]
+
+    result = free_dictionary.query(term)
+
+    if not result:
+        print(f'{term} definition not found')
+        sys.exit(2)
+
     word = dao.get_word_by_text(result.text)
 
-    dao.insert_definitions(word, result.definitions)
-    anki.add_note(word.text, result.definitions)
+    if not word:
+        print(f'Word {term} not found in database')
 
-anki.export_deck()
+        dao.insert_word(result)
+        word = dao.get_word_by_text(result.text)
+
+        dao.insert_definitions(word, result.definitions)
+        anki.add_note(word.text, result.definitions)
+    else:
+        print(f'Word {term} found in database. Nothing to do')
+
+    anki.export_deck()
